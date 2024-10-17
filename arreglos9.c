c8129edc0f65ecc127568ef7d35cf21fc95ef558
danieloab24 <b.abdaniel@comunidad.unam.mx>
2024-10-17 10:47:32 -0600

    Solucion de ejercicios del 6 al 9

--- arreglos9.c
+++ arreglos9.c
@@ -4,19 +4,27 @@ int main() {
     int A[2][2] = {{1, 2}, {3, 4}};
     int B[2][2] = {{5, 6}, {7, 8}};
     int C[2][2] = {0};  // Matriz resultante
+
+    int *ptrA, *ptrB, *ptrC;
+    ptrA= &A [0][0];
+    ptrB= &B [0][0];
+    ptrC= &C [0][0];
     
     // Multiplicación de matrices usando punteros
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
             for (int k = 0; k < 2; k++) {
                 // TODO: Realiza las operaciones solo usando apuntadores
+                *(ptrC+i*)
             }
         }
     }
     
     // Mostrar matriz resultante
     for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
+        printf("%d\t", arr[A][B][C]);
+        
+
             // TODO: Imprime los elementos de la matriz resultante, usando apuantadores
         }
         
