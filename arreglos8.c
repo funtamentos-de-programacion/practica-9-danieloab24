257f2a861aaa4e7fd8814f9e5b8b449a25f2adb9
danieloab24 <b.abdaniel@comunidad.unam.mx>
2024-10-17 23:59:03 -0600

    Update arreglos8.c

--- arreglos8.c
+++ arreglos8.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 
-int suma(int a[][3], int f);
-int suma2(int **a, intf, int c);
+int suma(int a[][3], int f);           
+int suma2(int **a, int f, int c);   
 
 int main() {
     int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-
-    int res = suma(arr, 3);
+    int res = suma(arr, 3);   
     int *ptr = &res;
-    printf("%d\t", *(ptr3));
-    //res= suma2(arr, 3, 3);
-    //printf("%d\t", *(ptr));
+    printf("%d\t", *ptr);
+
+    res = suma2(arr, 3, 3);
+    printf("%d\t", *ptr);
+
     return 0;
 }
-int suma2(int **a, int f, int c){
-    int r= 0;
-    for (int i = 0; i < f; i++) {
-        for(int j= 0; j < 3; j++); 
+
+int suma(int a[][3], int f) {
+    int r = 0;
+    for (int i=0; i<f; i++) {
+        for (int j=0; j<3; j++) {
             r += a[i][j];
         }
     }
     return r;
+}
+
+int suma2(int **a, int f, int c) {
+    int r = 0;
+    for (int i=0; i<f; i++) {
+        for (int j=0; j<c; j++) {  
+            r += a[i][j];
+        }
+    }
+    return r;
+}
+
 }
