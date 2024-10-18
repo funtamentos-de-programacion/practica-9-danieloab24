94cb9797ea12fdc38df2c7524cd8dcccd40460da
danieloab24 <b.abdaniel@comunidad.unam.mx>
2024-10-17 23:07:17 -0600

    Update arreglos5.c
    Solucion ejercicio 5

--- arreglos5.c
+++ arreglos5.c
@@ -2,11 +2,19 @@
 
 int main() {
     int a = 10, b = 20, c = 30;
-    //TODO: (14) Declara un arreglo de tres apuntadores a entero
-    
-    
-    //TODO: (15) Asigna a cada apuntador la dirección de las variables a, b y c
-    //TODO: (16) Mostrar los valores usando los punteros
-    
+
+    // (14) Declara un arreglo de tres apuntadores a entero
+    int *arr[3];
+
+    // (15) Asigna a cada apuntador la dirección de las variables a, b y c
+    arr[0] = &a;
+    arr[1] = &b;
+    arr[2] = &c;
+
+    // (16) Mostrar los valores usando los punteros
+    printf("Valor de a: %d\n", *arr[0]);
+    printf("Valor de b: %d\n", *arr[1]);
+    printf("Valor de c: %d\n", *arr[2]);
+
     return 0;
 }
