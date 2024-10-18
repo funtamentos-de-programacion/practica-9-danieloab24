0b09986a5efb79d8e1df8cb3ea8ad3d7499def64
danieloab24 <b.abdaniel@comunidad.unam.mx>
2024-10-17 22:28:24 -0600

    Update arreglos2.c
    Solucion arreglo 2

--- arreglos2.c
+++ arreglos2.c
@@ -1,11 +1,15 @@
+
+//TODO: (4) Declara un apuntador que apunte al primer elemento del arreglo#include <stdio.h>
 #include <stdio.h>
-
-int main(){
+int main() {
     int arr[]= {1, 2, 3, 4, 5};
-    //TODO: (4) Declara un apuntador que apunte al primer elemento del arreglo
-
-
+    int *ptr;
+    ptr = arr;
     //TODO: (5) Recorre el arreglo utilizando notación de punteros
+    printf("[ ");
+    for(int i =0; i < 5;i++)
+        printf("%d", *(ptr + i));
+    printf("]\n");
 
-    return 0;
+      return 0;
 }
