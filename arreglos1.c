6d6d43c61ae4f6d3ae46c373bd00314625e44345
danieloab24 <b.abdaniel@comunidad.unam.mx>
2024-10-17 22:19:12 -0600

    Update arreglos1.c
    Solucion ejercicio 1

--- arreglos1.c
+++ arreglos1.c
@@ -8,13 +8,24 @@ int main(){
     str2[0] = 'A';
     str2[1] = 'd';
     str2[2] = 'i';
-    str2[3] = 'ó';
-    str2[5] = 's';
+    str2[3] = 'o';
+    str2[4] = 's';
 
     // TODO: (1) Recorre arr mediante un índice usando un ciclo
-
+    printf("[");
+    for(int i=0; i < 5;i++)
+        printf("%d, ",arr [i]);
+    printf("]\n");
     // TODO: (2) Recorre str mediante un índice usando un ciclo
+    printf("[");
+    for(int i=0; i < 10;i++)
+        printf("%c, ",str [i]);
+    printf("]\n");
     
-
     // TODO:(3) Recorre str2 mediante un índice usando un ciclo
+       printf("[");
+    for(int i=0; i < 10;i++);
+        printf("%c, ",str2 [5]);
+    printf("]\n");
+    
 }
